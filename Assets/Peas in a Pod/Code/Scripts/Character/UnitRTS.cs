cd4ed901a58b2bcfa1c52b5a438725536badabdd
JonahFairweather <jonah.fairweather@gmail.com>
2022-11-15 16:10:50 -0500

    New Font and an imported farm package

--- Assets/Peas in a Pod/Code/Scripts/Character/UnitRTS.cs
+++ Assets/Peas in a Pod/Code/Scripts/Character/UnitRTS.cs
@@ -54,7 +54,11 @@ public class UnitRTS : MonoBehaviour
     {
         DesiredLocation = transform.position;
         CurrentHeat = StartingHeat;
-        HealthBar.SetHealth(CurrentHeat, MaximumHeat);
+        if (HealthBar != null)
+        {
+            HealthBar.SetHealth(CurrentHeat, MaximumHeat);
+        }
+        
         
     }
 
