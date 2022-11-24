f70505d05691caa4f9c7d8d2abf0ac0048688b70
Alexandre Barbeau <abarbeau14@gmail.com>
2022-11-24 10:15:23 -0500

    Revert "Dev Alex"

--- Assets/Billboard.cs
+++ Assets/Billboard.cs
@@ -9,6 +9,6 @@ public class Billboard : MonoBehaviour
 
     private void LateUpdate()
     {
-        //transform.LookAt(transform.position + cam.forward);
+        transform.LookAt(transform.position + cam.forward);
     }
 }
